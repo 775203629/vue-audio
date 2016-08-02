92cf17ce326b4eb6a461c1c31756b4eaaccf8f27
zzuieliyaoli <zzuieliyaoli@gmail.com>
2016-08-02 21:43:22 +0800

    add main function

--- src/entry.js
+++ src/entry.js
@@ -1,3 +1,38 @@
 import Vue from 'vue'
+import plugin from './index'
+import 'babel-polyfill'
 
-console.log(Vue)
+Vue.use(plugin)
+
+window.Vue = Vue
+
+new Vue({
+  el: '#root',
+  ready() {
+    this.$audio.init({
+      // autoplay: true
+    }, [
+      'http://music.default.kanjian.com/2014_12/110994_1418110939.mp3',
+      'http://music.default.kanjian.com/2016_02/118797_fd759ddbdbdf380c506c854be3e8d6bd1455581927.mp3',
+      'http://music.default.kanjian.com/2015_03/664153_1427693002.mp3'
+    ])
+  },
+  methods: {
+    play() {
+      console.log('asd')
+      this.$audio.play()
+    },
+    pause() {
+      this.$audio.pause()
+    },
+    keep() {
+      this.$audio.keep()
+    },
+    playNext() {
+      this.$audio.playNext()
+    },
+    playPrev() {
+      this.$audio.playPrev()
+    }
+  }
+})
--- src/index.js
+++ src/index.js
@@ -1,11 +1,95 @@
-import Vue fron 'vue'
+// @flow
+import { warn } from './util'
 
-Vue.use(asd)
+const plugin = {
+  install(Vue) {
+    Vue.prototype.$audio = {
+      audio: null,
+      index: 0,
+      currentTime: 0,
+      isPlayingAll: false,
+      sources: [],
+      // 初始化
+      init(config: object, sources) {
+        // create element
+        this._createAudio(config)
+        this._addEventHandler()
+        this.sources = sources
+      },
+      // 创建audio标签
+      _createAudio(config) {
+        const audioElement = document.createElement('audio')
+        // set config
+        for (const [key, value] of Object.entries(config)) {
+          if ((value !== undefined) && (value !== false)) {
+            audioElement.setAttribute(key, value)
+          }
+        }
+        this.audio = audioElement
+        document.body.appendChild(audioElement)
+      },
+      // 添加事件监听
+      _addEventHandler() {
+        this.audio.addEventListener('play', () => {
+          warn('play')
+        }, false)
+        this.audio.addEventListener('pause', () => {
+          warn('pause')
+        }, false)
+        this.audio.addEventListener('ended', () => {
+          warn('ended')
+          if (this.isPlayingAll === true) {
+            this.play(this.index + 1)
+          }
+        }, false)
+        this.audio.addEventListener('timeupdate', () => {
+          this.currentTime = this.audio.currentTime
+        }, false)
+      },
+      // 播放
+      play(index = 0) {
+        if ((index > this.sources.length - 1) || (index < 0)) {
+          warn(`the song's which you wanted to play is out of sources.`)
+          return false
+        }
+        this.index = index
+        this.audio.src = this.sources[this.index]
+        this.audio.play()
+      },
+      // 暂停
+      pause() {
+        this.audio.pause()
+      },
+      // 继续播放
+      keep() {
+        this.audio.play()
+      },
+      // 播放上一曲
+      playPrev() {
+        this.play(this.index - 1)
+      },
+      // 播放下一曲
+      playNext() {
+        this.play(this.index + 1)
+      },
+      // 播放全部
+      playAll() {
+        this.play()
+        this.isPlayingAll = true
+      },
+      // 歌曲开始播放时的回调
+      start() {
 
-var a = {
-  Vue
+      },
+      // 歌曲结束时的回调
+      end() {
+        // if (this.) {}
+      }
+      // 歌曲播放时间的回调
+      // 歌曲暂停时的回调
+      // 歌曲重新播放时的回调
+    }
+  }
 }
 
-export default a
-
-var b = 2
+export default plugin
