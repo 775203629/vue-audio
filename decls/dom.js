c74057563f353b2df0d84ee927a938f968fb615a
zzuieliyaoli <zzuieliyaoli@gmail.com>
2016-08-03 21:31:04 +0800

    add annotation

--- decls/dom.js
+++ decls/dom.js
@@ -1,3 +1,9 @@
+/**
+ * The type of `value` passed in `setAttribure` is string.
+ * However, According to the spec, the type of `value` is one of the Boolean、String、Number
+ * https://developer.mozilla.org/en-US/docs/Web/HTML/Element/audio
+ */
+
 declare class Element extends Node {
    setAttribute(name?: string, value?: mixed): void;
 }
